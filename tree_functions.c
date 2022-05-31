ef54ba55d6fa1eebf43c739768481d5f6ac45ca8
Frederico <fredericolarangeira@hotmail.com>
2022-05-31 11:50:34 -0300

    tree_init criado

--- tree_functions.c
+++ tree_functions.c
@@ -3,7 +3,14 @@
 #include "tree_def.h"
 #include "tree_operations.h"
 
-tree *tree_init(){
-    
+tree *tree_init()
+{
+    tree *root = malloc(sizeof(tree));
+    root = NULL;
+
+    return root;
 }
 
+tree *tree_insert(tree *root){
+    
+}
