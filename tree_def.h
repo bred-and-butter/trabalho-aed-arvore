6a1f7086d2e0a4458cbc8a77f678150ff9d29bed
Frederico <fredericolarangeira@hotmail.com>
2022-05-31 10:20:00 -0300

    definicoes iniciais

---
+++ tree_def.h
@@ -0,0 +1,20 @@
+#ifndef tree_definition
+#define tree_definition
+
+typedef struct tree
+{
+    char cpf[12];
+    char name[50];
+    unsigned int age;
+    struct tree *left_node;
+    struct tree *right_node;
+} tree;
+
+#endif
+
+#ifndef tree_operations
+#define tree_operations
+
+
+
+#endif
