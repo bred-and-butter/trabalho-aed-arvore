9747631fbdcad18f2360757ee84e02047f382fd8
Frederico <fredericolarangeira@hotmail.com>
2022-05-31 10:41:58 -0300

    definicoes da arvore

--- tree_def.h
+++ tree_def.h
@@ -1,9 +1,11 @@
+#define KEY_SIZE 12
+
 #ifndef tree_definition
 #define tree_definition
 
 typedef struct tree
 {
-    char cpf[12];
+    char cpf[KEY_SIZE];
     char name[50];
     unsigned int age;
     struct tree *left_node;
@@ -15,6 +17,20 @@ typedef struct tree
 #ifndef tree_operations
 #define tree_operations
 
+tree *tree_insert(tree *root, tree new_node);
 
+tree *tree_search(tree *root, char key[KEY_SIZE]);
+
+void tree_print(tree *root);
+
+void tree_print_in_order(tree *root);
+
+void tree_print_structure(tree *root);
+
+tree *tree_remove(tree *root, char key[KEY_SIZE]);
+
+int tree_node_amount(tree *root);
+
+int tree_height(tree *root)
 
 #endif
